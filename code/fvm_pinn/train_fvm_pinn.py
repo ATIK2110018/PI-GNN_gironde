4cc10e03df3587681c053c8d03ae9ef30f259393
ATIK2110018 <atikrahman282@gmail.com>
2026-08-13 23:51:27 +0600

    Set 50-epoch full training run, disable FAST_DEBUG_MODE

--- code/fvm_pinn/train_fvm_pinn.py
+++ code/fvm_pinn/train_fvm_pinn.py
@@ -62,7 +62,7 @@ def main():
     times_seconds = ds.variables['time'][:]
     ds.close()
     
-    FAST_DEBUG_MODE = True
+    FAST_DEBUG_MODE = False
     
     if FAST_DEBUG_MODE:
         print("\n!!! FAST DEBUG MODE ENABLED !!!")
@@ -222,9 +222,9 @@ def main():
     loss_history_phys = []
     
     if 'FAST_DEBUG_MODE' in locals() and FAST_DEBUG_MODE:
-        num_epochs = 1
+        num_epochs = 5
     else:
-        num_epochs = 60
+        num_epochs = 50
         
     total_t_steps = len(t_train_array)
     
